85317aa0afbf6e489f754d843fc548f914439041
Jacob Burton <jacobburton@gmail.com>
2018-04-06 11:04:58 -0600

    asdfsdf

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Product from '../Product/Product';
+
+class Dashboard extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h1>Dashboard Component</h1>
+                <Product />
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
